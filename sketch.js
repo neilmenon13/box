5ac22db0cc452acc02ccec6c319a678858b35d55
neilmenon13 <84827598+neilmenon13@users.noreply.github.com>
2021-10-05 19:00:02 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+
+  box = createSprite(200,200,30,30);
+}
+
+function draw() 
+{
+  background(30);
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    box.x = box.x + 5;
+  }
+  drawSprites();
+
+}
+
+
+
+
